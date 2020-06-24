f6c7d2caaf87d0524ae485ceffba5ab6a848bf91
Bart Joy <bart@ecargo.co.nz>
2020-06-25 07:21:45 +1200

    Add first cut of package for xsv

---
+++ xsv/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$workingDir = Join-Path $toolsDir "extract"
+$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  FileFullPath64 = Get-Item $toolsDir\*.zip
+  Destination = $workingDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+$extractedBinaryPath = Get-ChildItem -r $workingDir -fi xsv.exe | Select-Object -expand FullName
+Move-Item $extractedBinaryPath $toolsDir
+Remove-Item -r -fo $workingDir
