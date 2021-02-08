981579e1ebf98197b23606b59ae88b6dcf72db79
Bart Joy <bartjoy@gmail.com>
2021-02-08 16:24:16 +1300

    Create auto updating package for vifm
    In response to the discussion here:
    
    https://github.com/geotrader/chocolateyPackages/pull/3
    
    I have created this auto-updating version of the vifm Chocolatey package
    to hopefully reduce the manual maintenance required by maintainers.

---
+++ vifm/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$workingDir = Join-Path $toolsDir "extract"
+$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  FileFullPath64 = Get-Item $toolsDir\*.zip
+  Destination = $workingDir
+}
+
+if(Test-Path $workingDir) {
+  Remove-Item -Recurse -Force $workingDir
+}
+Get-ChocolateyUnzip @packageArgs
+New-Item "$(ls $workingDir -r win_helper.exe | select -Expand FullName).ignore"  # do not create shim for win_helper
